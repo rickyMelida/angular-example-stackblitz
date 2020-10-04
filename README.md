6c95b652720e3c482106a6092180248a644b4fb9
rickyMelida <ricardomelida92@gmail.com>
2020-10-04 08:43:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fqfijt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fqfijt)
---
+++ src/app/app.routing.ts
@@ -0,0 +1,8 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+
+export const appRoutingProviders : any[] = [];
+
+// export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutingProviders);
